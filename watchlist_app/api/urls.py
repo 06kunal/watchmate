ac515242d44b456ba868eb4d3fc61c9ce202810d
Kunal Garg <kt.garg437@gmail.com>
2026-02-07 00:03:04 +0530

    Added Review

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 #from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatformListAV, StreamPlatformDetailAV
+from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatformListAV, StreamPlatformDetailAV, ReviewList, ReviewDetail, ReviewCreate
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchListDetailAV.as_view(), name='movie-details'),
     path('platforms/', StreamPlatformListAV.as_view(), name='Platform-list'),
     path('platforms/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
-]
+    
+    # path('review/', ReviewList.as_view(), name='Review-list'),
+    # path('review/<int:pk>', ReviewDetail.as_view(), name='Review-detail'),
+    path('platforms/<int:pk>/review-create', ReviewCreate.as_view(), name='review-create'),
+    path('platforms/<int:pk>/review', ReviewList.as_view(), name='review-list'),
+    path('platforms/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    ]
