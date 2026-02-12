fa6a5bc7d0839d5f9c62b15686f321e63936e444
Kunal Garg <kt.garg437@gmail.com>
2026-02-12 23:20:43 +0530

    authentication added

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -110,33 +110,33 @@ class StreamPlatformVS(viewsets.ModelViewSet):
     
 
 
-''' 
-# Cannot have the same classnmae as model name.
-class StreamPlatformVS(viewsets.ViewSet):
-    """
-        A simple ViewSet for listing or retrieving users.
-    """
-    def list(self, request):
-        queryset = StreamPlatform.objects.all()
-        serializer = StreamPlatformSerializer(queryset, many=True)
-        return Response(serializer.data)
+# 
+# # Cannot have the same classnmae as model name.
+# class StreamPlatformVS(viewsets.ViewSet):
+#     """
+#         A simple ViewSet for listing or retrieving users.
+#     """
+#     def list(self, request):
+#         queryset = StreamPlatform.objects.all()
+#         serializer = StreamPlatformSerializer(queryset, many=True)
+#         return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = StreamPlatform.objects.all()
-        watchlist = get_object_or_404(queryset, pk=pk)
-        serializer = StreamPlatformSerializer(watchlist)
-        return Response(serializer.data)
+#     def retrieve(self, request, pk=None):
+#         queryset = StreamPlatform.objects.all()
+#         watchlist = get_object_or_404(queryset, pk=pk)
+#         serializer = StreamPlatformSerializer(watchlist)
+#         return Response(serializer.data)
     
-    def create(self, request):
-        serializer = StreamPlatformSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)   
+#     def create(self, request):
+#         serializer = StreamPlatformSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)   
         
-    #Similarly we can implement .destroy() function to delete. we just have to write the code and logic manually.     
-'''
+#     #Similarly we can implement .destroy() function to delete. we just have to write the code and logic manually.     
+# 
 
 class StreamPlatformListAV(APIView):
     permission_classes = [IsAdminOrReadOnly]
